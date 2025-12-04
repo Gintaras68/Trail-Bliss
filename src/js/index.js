37509be1a468d4ad7b8ff9f8e47f40155fc17db1
gintaras68 <gintaras5783@yahoo.com>
2025-12-04 17:00:16 +0200

    Swiper slider for OFFERS

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,36 @@ import { Fancybox } from '@fancyapps/ui';
 import '@fancyapps/ui/dist/fancybox/fancybox.css';
 
 Fancybox.bind('[data-fancybox]', {
-	// Your custom options
+  // Your custom options
 });
 
 // Мобильная навигация
 import mobileNav from './modules/mobile-nav.js';
 mobileNav();
+
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('#offers-swiper', {
+  loop: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  spaceBetween: 30,
+
+  breakpoints: {
+    820: {
+      centeredSlides: false,
+      slidesOffsetBefore: 20,
+      spaceBetween: 20,
+    },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '#offers-btn-next',
+    prevEl: '#offers-btn-prev',
+  },
+});
